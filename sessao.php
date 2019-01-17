4af5e67aba812720c029e527c9d8c0bda108537f
Denilson Lima da Costa <44618963+Denilsonlima@users.noreply.github.com>
2019-01-17 11:26:06 -0200

    Update sessao.php

--- sessao.php
+++ sessao.php
@@ -1,7 +1,9 @@
 <?php
 
+// lembrando que o arquivo 'config.php' deverá ser criado a parte confome o arquivo existente neste artigo.
+
 require_once("config.php");
 
-$_SESSION["nome"] = "Denilson";
+$_SESSION["nome"] = "Valor da sua variavel";
 
 ?>
