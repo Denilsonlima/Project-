012dc82cd54c68a3eb611df4c7b988a0c3fa93bc
Denilson Lima da Costa <44618963+Denilsonlima@users.noreply.github.com>
2019-01-17 11:28:27 -0200

    Update sessao2.php

--- sessao2.php
+++ sessao2.php
@@ -1,5 +1,7 @@
 <?php
 
+// neste arquivo utilizo o comando 'session_regenerate_id();' que é respondavel por gerar um novo id de sessão a cada atualização de página. 
+
 require_once("config.php");
 
 session_regenerate_id();
